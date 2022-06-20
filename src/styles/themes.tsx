5a9bf36449373231ed169093823f94bf28491b63
ttahm3d <ttahm3d@gmail.com>
2022-06-20 22:07:26 +0530

    feat: created color palette

--- src/styles/themes.tsx
+++ src/styles/themes.tsx
@@ -13,29 +13,41 @@ import {
   tealDark,
   plum,
   plumDark,
+  violetDark,
+  violet,
+  indigo,
+  indigoDark,
+  gray,
+  grayDark,
 } from "@radix-ui/colors";
 
 const LightTheme = {
   colors: {
     ...blue,
+    ...gray,
     ...plum,
     ...slate,
     ...teal,
     ...red,
     ...mauve,
     ...orange,
+    ...violet,
+    ...indigo,
   },
 };
 
 const DarkTheme = {
   colors: {
     ...blueDark,
+    ...grayDark,
     ...plumDark,
     ...slateDark,
     ...redDark,
     ...tealDark,
     ...mauveDark,
     ...orangeDark,
+    ...violetDark,
+    ...indigoDark,
   },
 };
 

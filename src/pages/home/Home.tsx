cfc08492ce5ab328bcc355d04eb092f01266fbff
ttahm3d <ttahm3d@gmail.com>
2022-06-22 21:20:28 +0530

    chore: renamde home page

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,10 +1,10 @@
 import styled from "styled-components";
 import { Container, Content } from "../../styles/globals";
 
-export default function Feed() {
+export default function Home() {
   return (
     <Content>
-      <h4>Feed</h4>
+      <h4>Home</h4>
     </Content>
   );
 }

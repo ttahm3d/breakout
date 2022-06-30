d034467fc3ce88b71e76c119ac2f6ea6cb152fc1
ttahm3d <ttahm3d@gmail.com>
2022-06-30 22:45:20 +0530

    type: created userType

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,3 +1,13 @@
 import * as FirebaseAuth from "firebase/auth";
 
 export interface IAuth extends FirebaseAuth.User {}
+
+export type UserType = {
+  userName: string;
+  password: "";
+  photoURL: string;
+  followers: [User];
+  following: [User];
+  bio: string;
+  website: string;
+};

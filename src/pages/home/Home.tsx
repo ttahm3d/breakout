25a3143626c31640fef078f8eabeec7a02f59851
ttahm3d <ttahm3d@gmail.com>
2022-07-02 18:28:58 +0530

    feat: removed uused code

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,11 +1,6 @@
-import { getAuth } from "firebase/auth";
 import { Content } from "../../styles/globals";
 
 export default function Home() {
-  const auth = getAuth();
-
-  console.log(auth.currentUser);
-
   return (
     <Content>
       <h4>Home</h4>

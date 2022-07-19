e86d39debadf68c89ff894ef852acf768ddadfb2
ttahm3d <ttahm3d@gmail.com>
2022-07-19 12:37:40 +0530

    feat: created Input component

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,10 +2,11 @@ import { Button } from "./Button/Button";
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import FormikField from "./FormikField/FormikField";
+import Input from "./Input/Input";
+import Loader, { LoaderCircle } from "./Loader/Loader";
 import Modal from "./Modal/Modal";
 import NavigationLink from "./Link/Link";
 import NavMenu from "./NavMenu/NavMenu";
-import Loader, { LoaderCircle } from "./Loader/Loader";
 import ScrollToTop from "./ScrollToTop/ScrollToTop";
 import Toast, { ToastComponent } from "./Toast/Toast";
 import UserCard from "./UserCard/Card";
@@ -15,6 +16,7 @@ export {
   Header,
   Footer,
   FormikField,
+  Input,
   Loader,
   LoaderCircle,
   Modal,

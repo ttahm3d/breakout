3685b7b35f3e0ee705a83b65c1af4a36b82e3e39
ttahm3d <ttahm3d@gmail.com>
2022-07-31 11:43:50 +0530

    feat: changed modal bgc

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -49,7 +49,7 @@ const StyledModalContainer = styled.div`
   height: 100vh;
   top: 0;
   left: 0;
-  background-color: rgba(22, 22, 24, 0.7);
+  background-color: rgba(132, 79, 132, 0.4);
   display: flex;
   align-items: center;
   justify-content: center;
